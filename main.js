38e359d5e5779e081b62a2239ec5a8b9ad18d8b3
phuocduydev <phuocduy.dev@gmail.com>
2023-07-28 12:42:37 +0700

    feat: add navbar-8

--- main.js
+++ main.js
@@ -11,7 +11,20 @@ navItemList2.forEach(n => {
 })
 /** END NAV ITEM 2 */
 
-/** BEGIN NAV ITEM 2 */
+/** BEGIN NAV ITEM 8 */
+const navItemList8 = document.querySelectorAll('.nav-item-8')
+
+navItemList8.forEach(n => {
+  n.addEventListener("click", () => {
+    navItemList8.forEach(n2 => {
+      n2.classList.remove('active')
+    })
+    n.classList.add('active')
+  })
+})
+/** END NAV ITEM 8 */
+
+/** BEGIN NAV ITEM 10 */
 const navItemList10 = document.querySelectorAll('.nav-item-10')
 
 navItemList10.forEach(n => {
@@ -22,4 +35,4 @@ navItemList10.forEach(n => {
     n.classList.add('active')
   })
 })
-/** END NAV ITEM 2 */
+/** END NAV ITEM 10 */
