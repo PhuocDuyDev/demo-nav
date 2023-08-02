a651308d3d9ba0a9b3073ce0fbec8f22f0b0a062
Mon <thanhngan10604@gmail.com>
2023-08-02 21:58:27 +0700

    feat: done navbar 4

--- main.js
+++ main.js
@@ -1,38 +1,51 @@
 /** BEGIN NAV ITEM 2 */
-const navItemList2 = document.querySelectorAll('.nav-item-2')
+const navItemList2 = document.querySelectorAll(".nav-item-2");
 
-navItemList2.forEach(n => {
-  n.addEventListener("click", () => {
-    navItemList2.forEach(n2 => {
-      n2.classList.remove('active')
-    })
-    n.classList.add('active')
-  })
-})
+navItemList2.forEach((n) => {
+    n.addEventListener("click", () => {
+        navItemList2.forEach((n2) => {
+            n2.classList.remove("active");
+        });
+        n.classList.add("active");
+    });
+});
 /** END NAV ITEM 2 */
 
-/** BEGIN NAV ITEM 8 */
-const navItemList8 = document.querySelectorAll('.nav-item-8')
+/** BEGIN NAV ITEM 4 */
+const navItemList4 = document.querySelectorAll(".nav-item-4");
 
-navItemList8.forEach(n => {
-  n.addEventListener("click", () => {
-    navItemList8.forEach(n2 => {
-      n2.classList.remove('active')
-    })
-    n.classList.add('active')
-  })
-})
+navItemList4.forEach((n) => {
+    n.addEventListener("click", () => {
+        navItemList4.forEach((n2) => {
+            n2.classList.remove("active");
+        });
+        n.classList.add("active");
+    });
+});
+/** END NAV ITEM 4 */
+
+/** BEGIN NAV ITEM 8 */
+const navItemList8 = document.querySelectorAll(".nav-item-8");
+
+navItemList8.forEach((n) => {
+    n.addEventListener("click", () => {
+        navItemList8.forEach((n2) => {
+            n2.classList.remove("active");
+        });
+        n.classList.add("active");
+    });
+});
 /** END NAV ITEM 8 */
 
 /** BEGIN NAV ITEM 10 */
-const navItemList10 = document.querySelectorAll('.nav-item-10')
+const navItemList10 = document.querySelectorAll(".nav-item-10");
 
-navItemList10.forEach(n => {
-  n.addEventListener("click", () => {
-    navItemList10.forEach(n2 => {
-      n2.classList.remove('active')
-    })
-    n.classList.add('active')
-  })
-})
+navItemList10.forEach((n) => {
+    n.addEventListener("click", () => {
+        navItemList10.forEach((n2) => {
+            n2.classList.remove("active");
+        });
+        n.classList.add("active");
+    });
+});
 /** END NAV ITEM 10 */
