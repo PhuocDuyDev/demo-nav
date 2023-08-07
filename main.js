c8266da1927bc024218163370dc928df9b423164
Mon <thanhngan10604@gmail.com>
2023-08-07 21:27:56 +0700

    mon done navbar 7

--- main.js
+++ main.js
@@ -76,6 +76,19 @@ navItemList6.forEach((n) => {
 });
 /** END NAV ITEM 6 */
 
+/** BEGIN NAV ITEM 7 */
+const navItemList7 = document.querySelectorAll(".nav-item-6");
+
+navItemList6.forEach((n) => {
+  n.addEventListener("click", () => {
+    navItemList7.forEach((n2) => {
+      n2.classList.remove("active");
+    });
+    n.classList.add("active");
+  });
+});
+/** END NAV ITEM 7*/
+
 /** BEGIN NAV ITEM 8 */
 const navItemList8 = document.querySelectorAll(".nav-item-8");
 
